a7fec9ae41c5cf3ccee2fa0ddfedef664ee01587
AMZ-jpcslr <is0772vk@ed.ritsumei.ac.jp>
2025-06-27 20:05:58 +0900

    slash command update

---
+++ src/deploy-commands.ts
@@ -0,0 +1,28 @@
+import { REST, Routes, SlashCommandBuilder } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const commands = [
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Botの応答速度を測定します。')
+        .toJSON(),
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN as string)
+
+async function main() {
+    try {
+        console.log('スラッシュコマンドを登録中...')
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID as string),
+            { body: commands }
+        )
+        console.log('登録完了！')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+main()
